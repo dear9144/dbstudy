7b4f083b98c5c2d329eb2ef49a672b2350277103
dear9144 <dear9144@naver.com>
2023-02-02 10:38:08 +0900

    2023-02-02

---
+++ Day02_01_계정만들기.sql
@@ -0,0 +1,13 @@
+    -- 단일주석
+    /*
+    자바랑 똑같음 
+    */
+    
+    --노션
+    
+    --
+    DROP USER GDJ61 CASCADE;
+    CREATE USER GDJ61 IDENTIFIED BY 1111;
+    GRANT DBA TO GDJ61;
+    
+    
